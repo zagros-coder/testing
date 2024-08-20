456a0eaebbfce24c113422f308d14658123a9f7f
Ahmed <ahmedhatem1470@gmail.com>
2024-08-21 00:18:11 +0300

    adding a new function

---
+++ main.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+ let p = document.createElement("p");
+ let pText = document.createTextNode("ahmed haten mohamed saied salem ali");
+ p.append(pText);
+ if(p.length > 1){
+  console.log("ahmed is the best programmer");
+ }else{
+     console.log("are you good ???? (:");
+};
+}
